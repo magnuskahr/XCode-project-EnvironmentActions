3399917ac1479a9adcd8bcbcfba35264545b6dbe
Magnus Jensen <mkj@vertica.dk>
2021-10-24 11:19:30 +0200

    Initial Commit

---
+++ EnvironmentActions/EnvironmentActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentActionsApp.swift
+//  EnvironmentActions
+//
+//  Created by Magnus Jensen on 24/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
